871f4024758507b6c607583d6996ec93d764a528
Juesonghyeon <199971alfks@gmail.com>
2025-08-06 21:43:06 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import BasicJsx from './components/BasicJsx/BasicJsx'
+import BasicProps from './components/BasicProps/BasicProps'
+import BasicReact from './components/BasicReact'
+import CountState from './components/BasicState/CountState/CountState'
+
+function App() {
+
+  return (
+    <>
+      {/* <BasicJsx /> */}
+      {/* <BasicProps/> */}
+      <CountState/>
+    </>
+  )
+}
+
+export default App
