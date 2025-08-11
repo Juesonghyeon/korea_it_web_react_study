57348a1ae265785f477a40f5bcc226fc13aba9df
Juesonghyeon <199971alfks@gmail.com>
2025-08-11 21:46:19 +0900

    Feat: BasicState

---
+++ src/components/BasicState/CountButton/CountButton.jsx
@@ -0,0 +1,10 @@
+
+function CountButton({ value, onClick, text }) {
+	return (
+		<button value={value} onClick={onClick}>
+			{text}
+		</button>
+	);
+}
+
+export default CountButton;
---
+++ src/components/LifeCycle/Render.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function Render() {
+    console.log("Render 컴포넌트가 랜더링 되었습니다.");
+    
+    const [count, setCount] = useState(0);
+    const onClickHandler = () => {
+        setCount(count + 1);
+    };
+    //랜더링이라는 것은 해당 컴포넌트의 내용물이 새로운 값으로 교체 되는 것을 의미
+    //예를 들어서 컴포넌트가 건물이라고 한다면, 랜더링은 건물 내부의 인테리어를 바꾸는 것
+    
+  return (
+    <div>
+        <h1>현재 숫자: {count}</h1>
+        <button onClick={onClickHandler}>숫자 증가</button>
+    </div>
+  )
+}
+
+export default Render
